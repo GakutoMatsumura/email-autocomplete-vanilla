fe849b777b3dbbbdaa404c255a85dc2dd32c4e45
Gakuto Matsumura <99163665+GakutoMatsumura@users.noreply.github.com>
2023-02-27 10:58:02 +0900

    first release
    first release

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# email-autocomplete-vanilla
+# email-autocomplete-vanilla.js
  Vanilla JS version of email-autocomplete that suggests and autocompletes domains as the user types in the email address field.
+
+----
+email-autocomplete, Vanilla JS full-remake.
+
+## What is this?
+----
+This javascript is a full-remake of [Low Yong Zhen's](https://github.com/yongzhenlow/email-autocomplete) 1.3.0 contents into Vanilla JS (pure javascript) that does not depend on jQuery.
+
+
+## What does it do?
+----
+If the user types "user@gm", the script suggests: "user@gmail.com", based on the first result from the list of predefined email domains.
+
+----
+
+### Download
+Download or clone this repo and copy dist/email-autocomplete-vanilla.min.js into your javascripts directory.
+
+
+## Usage
+----
+```javascript
+<script src="email-autocomplete-vanilla.min.js"></script>
+```
+
+```html
+<input id="target_input_id" name="email" type="email">
+```
+
+```html
+<script>
+document.addEventListener("DOMContentLoaded", function(e) {
+	emailautocomplete("#target_input_id", {
+		domains: ["gmail.com","icloud.com"],
+		suggClass: "custom-classname",
+		suggBkColor: "inherit"
+	});
+});
+</script>
+```
+
+## Settings
+----
+| Option | Type | Default | Description |
+| --- | --- | --- | --- |
+| suggClass | string | 'eac-sugg' | Classname for the suggestion text element. |
+| domains | array | See list of domains below | Array of domains used for autocompleting. |
+
+## Styling
+----
+Use the following CSS to style the suggestion text color. Remember to update the classname if you've changed it to a custom one.
+
+```css
+.eac-sugg {
+	color: #ccc;
+}
+```
+
+## domains
+----
+These are the plugin default domains if the domains option is not supplied.
+
+* yahoo.co.jp
+* gmail.com
+* ezweb.ne.jp
+* au.com
+* docomo.ne.jp
+* i.softbank.jp
+* softbank.ne.jp
+* excite.co.jp
+* googlemail.com
+* hotmail.co.jp
+* hotmail.com
+* icloud.com
+* live.jp
+* me.com
+* mineo.jp
+* nifty.com
+* outlook.com
+* outlook.jp
+* yahoo.ne.jp
+* ybb.ne.jp
+* ymobile.ne.jp
+
+## Author
+----
+### Remake
+* Gakuto Matsumura
+
+### Origin
+* Low Yong Zhen
+thank you.
+
+## LICENSE
+----
+MIT License
+
---
+++ src/email-autocomplete-vanilla.js
@@ -0,0 +1,215 @@
+/*! email-autocomplete-vanilla 0.1.3.1
+ * MIT License
+ * (c) 2023 Gakuto Matsumura (http://github.com/GakutoMatsumura)
+ * (c) 2014 Low Yong Zhen <yz@stargate.io>
+*/
+"use strict";
+
+(function (window, document) {
+	function EmailAutocompleteVanilla(obj, options) {
+		if (!obj) {
+			console.error("autoemail,const:C00");
+			return;
+		}
+		var defaults = {
+			suggClass: "eac-sugg",
+			// domains japan region
+			domains: ['yahoo.co.jp', 'gmail.com', 'ezweb.ne.jp', 'au.com', 'docomo.ne.jp', 'i.softbank.jp', 'softbank.ne.jp', 'excite.co.jp', 'googlemail.com', 'hotmail.co.jp', 'hotmail.com', 'icloud.com', 'live.jp', 'me.com', 'mineo.jp', 'nifty.com', 'outlook.com', 'outlook.jp', 'yahoo.ne.jp', 'ybb.ne.jp', 'ymobile.ne.jp']
+		};
+		this._f = obj;
+		this._o = Object.assign({}, defaults, options);
+		this._d = this._o.domains;
+		this.init();
+	}
+
+	EmailAutocompleteVanilla.prototype = {
+		init: function () {
+			try {
+				//shim indexOf
+				if (!Array.prototype.indexOf) this.doIndexOf();
+
+				//this will be calculated upon keyup
+				this._flo = null;
+
+				var field_obj = this._f;
+				const field_style = (field_obj ? getComputedStyle(field_obj) : {}) || {};
+				if (Object.keys(field_style).length === 0) {
+					console.error('autoemail,init:C01');
+					return;
+				}
+				if (!field_obj.parentNode) {
+					console.error('autoemail,init:C02');
+					return;
+				}
+
+				//wrap our field
+				var wrap = document.createElement("div");
+				wrap.className = "eac-input-wrap";
+				if (wrap.style) {
+					wrap.style.display = field_style.display;
+					wrap.style.position = (field_style.position === "static" ? "relative" : field_style.position);
+					wrap.style.fontSize = field_style.fontSize;
+					wrap.style.width = field_style.width;
+				}
+
+				field_obj.parentNode.insertBefore(wrap, field_obj);
+				wrap.appendChild(field_obj);
+
+				//create container to test width of current val
+				this._cv = document.createElement("span");
+				var cval_obj = this._cv;
+				cval_obj.className = "eac-cval";
+				var cval_style = cval_obj.style || {};
+				cval_style.visibility = "hidden";
+				cval_style.position = "absolute";
+				cval_style.display = "inline-block";
+				cval_style.fontFamily = field_style.fontFamily;
+				cval_style.fontWeight = field_style.fontWeight;
+				cval_style.letterSpacing = field_style.letterSpacing;
+				field_obj.parentNode.insertBefore(cval_obj, field_obj.nextSibling);
+
+				//create the suggestion overlay
+				this._so = document.createElement("span");
+				var suggest_overlay_obj = this._so;
+				if (this._o)  suggest_overlay_obj.className = this._o.suggClass;
+				var suggest_overlay_style = suggest_overlay_obj.style || {};
+				suggest_overlay_style.display = "block";
+				suggest_overlay_style.boxSizing = "content-box";
+				suggest_overlay_style.lineHeight = field_style.lineHeight;
+
+				// paddingTop to marginTop
+				suggest_overlay_style.marginTop = parseFloat(field_style.paddingTop) + ((parseFloat(field_obj.offsetHeight) - parseFloat(field_obj.clientHeight)) / 2) + 'px';
+				suggest_overlay_style.marginBottom = suggest_overlay_style.marginTop;
+				suggest_overlay_style.fontFamily = field_style.fontFamily;
+				suggest_overlay_style.fontWeight = field_style.fontWeight;
+				suggest_overlay_style.letterSpacing = field_style.letterSpacing;
+				suggest_overlay_style.position = "absolute";
+				suggest_overlay_style.top = "0";
+				suggest_overlay_style.left = "0";
+
+				field_obj.parentNode.insertBefore(this._so, field_obj.nextSibling);
+
+				//bind events and handlers (Enter key add)
+				field_obj.addEventListener("keyup", this.displaySuggestion.bind(this));
+				field_obj.addEventListener("blur", this.autocomplete.bind(this));
+				field_obj.addEventListener("keydown", (function (e) { if (e.which === 13/*Enter*/ || e.which === 39/*→*/ || e.which === 9/*Tab*/) this.autocomplete(); }).bind(this));
+
+				suggest_overlay_obj.addEventListener("mousedown", this.autocomplete.bind(this));
+				suggest_overlay_obj.addEventListener("touchstart", this.autocomplete.bind(this));
+			} catch (e) {
+				console.error('autoemail,init:', e);
+			}
+		},
+		suggest: function (str) {
+			if (!str || !this._d || !Array.isArray(this._d)) {
+				return "";
+			}
+
+			var str_arr = str.split("@");
+			if (str_arr.length > 1) {
+				str = str_arr.pop();
+				if (!str.length) {
+					return "";
+				}
+			}
+			else {
+				return "";
+			}
+
+			var match = this._d.filter(function (domain) {
+				return domain.indexOf(str) === 0;
+			}).shift() || "";
+
+			return match ? match.replace(str, "") : "";
+		},
+
+		autocomplete: function () {
+			if (!this._s || this._s.length < 1 || !this._v) {
+				return false;
+			}
+			if (this._f) this._f.value = this._v + this._s;
+			if (this._so) this._so.textContent = "";
+			if (this._cv) this._cv.textContent = "";
+			return true;
+		},
+
+		/**
+		 * Displays the suggestion, handler for field keyup event
+		 */
+		displaySuggestion: function (e) {
+			this._v = (this._f ? this._f.value : '');
+			this._s = this.suggest(this._v);
+
+			if (!this._s.length) {
+				this._so.textContent = "";
+			} else {
+				e.preventDefault();
+			}
+
+			//update with new suggestion
+			if (this._so) this._so.textContent = this._s;
+			if (this._cv) this._cv.textContent = this._v;
+
+			// get input padding, border and margin to offset text
+			if (this._flo === null) {
+				const style = (this._f && getComputedStyle(this._f)) || {};
+				this._flo = (parseFloat(style.paddingLeft) + parseFloat(style.borderLeftWidth)) || 0;
+			}
+
+			//left position suggestion text
+			var left = parseFloat(this._flo) + parseFloat(this._cv && this._cv.getBoundingClientRect ? this._cv.getBoundingClientRect().width : 0);
+			if (!isNaN(left)) {
+				this._so.style.left = left + "px";
+			}
+		},
+
+		/**
+		 * indexof polyfill
+		 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf#Polyfill
+		*/
+		doIndexOf: function () {
+			Array.prototype.indexOf = function (searchElement, fromIndex) {
+				if (this === undefined || this === null) {
+					throw new TypeError('"this" is null or not defined');
+				}
+
+				var length = this.length >>> 0; // Hack to convert object.length to a UInt32
+
+				fromIndex = +fromIndex || 0;
+
+				if (Math.abs(fromIndex) === Infinity) {
+					fromIndex = 0;
+				}
+
+				if (fromIndex < 0) {
+					fromIndex += length;
+					if (fromIndex < 0) {
+						fromIndex = 0;
+					}
+				}
+
+				for (; fromIndex < length; ++fromIndex) {
+					if (this[fromIndex] === searchElement) {
+						return fromIndex;
+					}
+				}
+				return -1;
+			};
+		}
+	};
+
+	window.emailautocomplete = function (selector, options) {
+		var elems = document.querySelectorAll(selector);
+		var instances = [];
+		if (elems) {
+			for (var i = 0; i < elems.length; ++i) {
+				if (!elems[i].set_emailautocomplete) {
+					instances.push(new EmailAutocompleteVanilla(elems[i], options));
+					elems[i].set_emailautocomplete = true;
+				}
+			}
+		}
+		return instances;
+	};
+
+})(window, document);
